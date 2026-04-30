40a121d73b7fcf7ae02f72ce1b7dd44933abcffe
isis <Isiaraujo0609@gmail.com>
2026-04-29 21:32:13 -0300

    imparoupar.py

---
+++ imparoupar.py
@@ -0,0 +1,5 @@
+num1 = int(input("Digite o primeiro número aqui: "))
+if num1 % 2 == 0:
+ print(f"O número {num1} é par e elevado ao quadrado é {num1**2}")
+
+else: print(f"O número {num1} é impar e elevado ao cubo é {num1**3}")
